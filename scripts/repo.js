309f5bfa689fd821449a827c15122a036089d304
YC <yamile.colque@gmail.com>
2016-10-30 14:17:26 -0700

    code works, loads on 3000

--- scripts/repo.js
+++ scripts/repo.js
@@ -3,20 +3,30 @@
 
   repos.all = [];
 
-  // TODO: Refactor and simplify this ajax call into a get request
-  // to the proxy end point provided by server.js.
-  repos.requestRepos = function(callback) {
-    $.ajax({
-      url: 'https://api.github.com/users/brookr/repos' +
-            '?per_page=100' +
-            '&sort=updated',
-      type: 'GET',
-      headers: { 'Authorization': 'token ' + githubToken },
-      success: function(data, message, xhr) {
-        repos.all = data;
-      }
+  // // TODO: Refactor and simplify this ajax call into a get request
+  // // to the proxy end point provided by server.js.
+  // repos.requestRepos = function(callback) {
+  //   $.ajax({
+  //     url: 'https://api.github.com/users/repos' +
+  //           '?per_page=100' +
+  //           '&sort=updated',
+  //     type: 'GET',
+  //     headers: { 'Authorization': 'token ' + githubToken },
+  //     success: function(data, message, xhr) {
+  //       repos.all = data;
+  //     }
+  //   }).done(callback);
+  // };
+
+  repos.requestRepos = function(callback){
+    $.get('https://api.github.com/users/YC2015/repos' +
+              '?per_page=100' +
+             '&sort=updated', function(data){
+               repos.all = data;
     }).done(callback);
   };
+
+
 
   repos.with = function(attr) {
     return repos.all.filter(function(repo) {
